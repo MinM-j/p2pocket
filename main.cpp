4400b427e7ff83ec6ccd4a938baa4437373647f9
mandip1213 <thapamandip1213@gmail.com>
2024-02-24 01:51:45 +0545

    started working on storage

--- main.cpp
+++ main.cpp
@@ -1,21 +1,26 @@
 #include<iostream>
+#include<fstream>
 #include<random>
 #include<functional>
 #include<bitset>
 #include<string>
 #include"./include/routing_table.h"
 #include"./include/id.h"
+#include"./include/storage/sha1.h"
+#include"./include/storage/storage.h"
 
 int main(){
   //std::cout<<generate_id()<<std::endl;
 
-  RoutingTable rt{"101100"};
+
+  std::cout<<fs::file_size("hello.txt")<<std::endl;
+
+  storage::store_file("hello.txt");
 
 
-  rt.print();
-  ID id{"101101"};
-  rt.insert_node(id, 234);
-  rt.print();
+
+    
+
 
   //std::cout<<rt<<std::endl;
 
